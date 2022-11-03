92b071ed7896746c522603451e3e9baa72bd042b
Jens Blawatt <jens.blawatt@codecentric.de>
2022-11-03 09:31:27 +0100

    fix(nvim-tree): set configuration

--- nvim-lua/lua/j3nko/plugins/nvim-tree.lua
+++ nvim-lua/lua/j3nko/plugins/nvim-tree.lua
@@ -7,7 +7,59 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvim_tree.setup({})
+nvim_tree.setup({
+  disable_netrw       = false, -- default true
+  hijack_netrw        = false, -- default true
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+    exclude = {"__pycache__"}
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    hide_root_folder = false,
+    side = 'left',
+    mappings = {
+      custom_only = false,
+      list = {}
+    },
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes"
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  }
+})
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>")
