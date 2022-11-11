11fb1c48b289f65f6ac9ecfd741a01e60c7dfd36
Jens Blawatt <jblawatt@googlemail.com>
2022-11-11 23:42:24 +0100

    improve commands

--- nvim-lua/lua/j3nko/core/user-commands.lua
+++ nvim-lua/lua/j3nko/core/user-commands.lua
@@ -1,24 +1,45 @@
 
 vim.api.nvim_create_user_command("Trun", function(opts)
-    vim.cmd ("split term://" .. opts.args)
+    vim.cmd ("term " .. opts.args)
 end, {nargs = 1})
+
 
 vim.api.nvim_create_user_command("TrunV", function(opts)
     vim.cmd ("vsplit term://" .. opts.args)
 end, {nargs = 1})
 
-vim.api.nvim_create_user_command("TrunMake", function(opts)
-    vim.cmd ("split term://make " .. opts.args)
+
+vim.api.nvim_create_user_command("TrunS", function(opts)
+    vim.cmd ("split term://" .. opts.args)
 end, {nargs = 1})
 
+
+vim.api.nvim_create_user_command("TrunMake", function(opts)
+    vim.cmd ("term make " .. opts.args)
+end, {nargs = 1})
+
+
 vim.api.nvim_create_user_command("TrunPy", function(opts)
+    vim.cmd ("term python " .. opts.args)
+end, {nargs = 1})
+
+
+vim.api.nvim_create_user_command("TrunSPy", function(opts)
     vim.cmd ("split term://python " .. opts.args)
 end, {nargs = 1})
+
+
+vim.api.nvim_create_user_command("TrunVPy", function(opts)
+    vim.cmd ("vsplit term://python " .. opts.args)
+end, {nargs = 1})
+
 
 vim.api.nvim_create_user_command("TrunVMake", function(opts)
     vim.cmd ("vsplit term://make " .. opts.args)
 end, {nargs = 1})
 
-vim.api.nvim_create_user_command("TrunVPy", function(opts)
-    vim.cmd ("vsplit term://python " .. opts.args)
+
+vim.api.nvim_create_user_command("TrunSMake", function(opts)
+    vim.cmd ("split term://make " .. opts.args)
 end, {nargs = 1})
+
