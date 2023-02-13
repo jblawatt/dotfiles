7a99d0419f115e383e24b242fbba933d027ea1b5
Jens Blawatt <jblawatt@googlemail.com>
2023-02-13 20:11:34 +0100

    merge

--- nvim-lua/lua/j3nko/core/colorscheme.lua
+++ nvim-lua/lua/j3nko/core/colorscheme.lua
@@ -1,17 +1,17 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
 -- local colorscheme = "gruber"
-local colorscheme = "oxocarbon"
+-- local colorscheme = "oxocarbon"
 -- local colorscheme = "poe"
 -- local colorscheme = "gruber"
 
-<<<<<<< HEAD
-=======
 -- local colorscheme = "gruber"
-local colorscheme = "material"
+-- local colorscheme = "material"
+local colorscheme = "gruber"
 -- local colorscheme = "dracula"
 
->>>>>>> 1fc3f62 (changes)
+-- local colorscheme = "oxocarbon"
+
 local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
