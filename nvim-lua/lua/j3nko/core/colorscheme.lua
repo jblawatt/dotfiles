af01104952d3f2dbae158878c235ea1da5361627
Jens Blawatt <jens.blawatt@codecentric.de>
2023-04-03 10:38:59 +0200

    wip

--- nvim-lua/lua/j3nko/core/colorscheme.lua
+++ nvim-lua/lua/j3nko/core/colorscheme.lua
@@ -6,13 +6,10 @@
 -- local colorscheme = "gruber"
 -- local colorscheme = "gruber"
 -- local colorscheme = "material"
-<<<<<<< HEAD
 -- local colorscheme = "ambiance"
 local colorscheme = "paramount"
-=======
-local colorscheme = "ambiance"
+-- local colorscheme = "ambiance"
 -- local colorscheme = "vscode"
->>>>>>> 291ec0c (config vim wip)
 -- local colorscheme = "dracula"
 
 local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
--- nvim-lua/lua/j3nko/plugins-setup.lua
+++ nvim-lua/lua/j3nko/plugins-setup.lua
@@ -74,6 +74,7 @@ return packer.startup(function(use)
     use("vim-scripts/kiss.vim")
     use("kwsp/halcyon-neovim")
     use("teloe/bland.vim")
+<<<<<<< HEAD
     use("perfectspr/dracula-vim")
     -- monochrome
     use("owickstrom/vim-colors-paramount")
@@ -81,6 +82,9 @@ return packer.startup(function(use)
     use("n1ghtmare/noirblaze-vim")
     use("axvr/photon.vim")
     use { "https://git.sr.ht/~toastal/sugilite256", branch = "trunk", rtp = "vim" }
+=======
+    -- use("perfectspr/dracula-vim")
+>>>>>>> 134c799 (wip)
     -- < colorshemes
 
     use("szw/vim-maximizer") -- maximizes and restores current window
